a049d1a7c9fa011845e00d58e03841f3214fefa7
Jason <hlbj105@qq.com>
2022-11-25 11:43:43 +0800

    🐳 chore: 修复构建失败

--- components.d.ts
+++ components.d.ts
@@ -25,6 +25,7 @@ declare module '@vue/runtime-core' {
     ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElResult: typeof import('element-plus/es')['ElResult']
     ElRow: typeof import('element-plus/es')['ElRow']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
